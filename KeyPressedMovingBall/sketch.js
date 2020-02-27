14f5874cbc690273c158fd619bc511753aa06e5a
ErikOnsager <60490219+ErikOnsager@users.noreply.github.com>
2020-02-27 14:06:28 -0800

    Update sketch.js

--- KeyPressedMovingBall/sketch.js
+++ KeyPressedMovingBall/sketch.js
@@ -5,6 +5,15 @@ posx=50
 posy=50
 
 function draw(){
+  var d = dist(mouseX, mouseY, posx, posy);
+  if (d < 30) {
+    fill(255, 0, 0);
+  } else {
+    fill(0, 255, 0);
+  }
+  
+  
+  
   background(100,0,0)
   ellipse(posx,posy,50,50);         //Define ellipse with position variables
   if (keyIsPressed && key == 'd' && posx<=375) { //Control movement with wasd by changing posx&y
