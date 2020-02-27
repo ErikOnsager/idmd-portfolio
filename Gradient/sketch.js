c220abb9e7fb4ed7fec257125bb55fb1bfcc4665
ErikOnsager <60490219+ErikOnsager@users.noreply.github.com>
2020-02-27 14:15:13 -0800

    Update sketch.js

--- Gradient/sketch.js
+++ Gradient/sketch.js
@@ -1,10 +1,12 @@
 function setup() {
   createCanvas(700, 700);
 }
+k=0;
 
 function draw() {
-  for (k = 0; k < 700; k++) { //For loop to draw 700 lines that slightly change color
+  while (k < 700) { //For loop to draw 700 lines that slightly change color
     stroke(k*0.9, k*0.25, 205);    //color
     line(0, k, 700, k);   //drawing the line at the y-level k
+    k++
   }
 }
