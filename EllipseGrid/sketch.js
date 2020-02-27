1065f591f531357eb1d44c5d040c5e17455683e4
ErikOnsager <60490219+ErikOnsager@users.noreply.github.com>
2020-02-27 14:11:18 -0800

    Update sketch.js

--- EllipseGrid/sketch.js
+++ EllipseGrid/sketch.js
@@ -1,13 +1,15 @@
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(700, 700);
+  widthvar = (width/100)+50
+  heightvar = (height/100)+50
 }
 
 function draw() {
   background([0, 100, 100, 7]);     //writing background color
   
-  for (var b=0; b<9; ++b)           //creating an ellipse for an x&y grid
+  for (var b=0; b<widthvar; ++b)           //creating an ellipse for an x&y grid
   {
-  	for (z=0; z<10; ++z)
+  	for (z=0; z<heightvar; ++z)
   	{
       fill ([z*20+b*10, 0, 100, 9]);     //writing color for each ball
     	ellipse(z*100, b*100, 100, 100);
