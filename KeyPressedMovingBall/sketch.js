bce96b9c18ccd8ca38305f92299520dc44b1adbe
ErikOnsager <60490219+ErikOnsager@users.noreply.github.com>
2020-03-02 11:32:26 -0800

    Update sketch.js

--- KeyPressedMovingBall/sketch.js
+++ KeyPressedMovingBall/sketch.js
@@ -1,31 +1,32 @@
-function setup() {
-  createCanvas(400, 400);
-}
-posx=50
-posy=50
 
-function draw(){
+function setup() {
+  createCanvas(500,500);
+}
+posx = 50
+posy = 50
+
+function draw() {
   var d = dist(mouseX, mouseY, posx, posy);
   if (d < 30) {
     fill(255, 0, 0);
   } else {
     fill(0, 255, 0);
   }
-  
-  
-  
-  background(100,0,0)
-  ellipse(posx,posy,50,50);         //Define ellipse with position variables
-  if (keyIsPressed && key == 'd' && posx<=375) { //Control movement with wasd by changing posx&y
-    posx+=2;
+
+
+
+  background(100, 0, 0)
+  ellipse(posx, posy, 50, 50); //Define ellipse with position variables
+  if (keyIsPressed && keyCode == RIGHT_ARROW && posx <= width-25) { //Control movement with wasd by changing posx&y
+    posx += 2;
   }
-  if (keyIsPressed && key == 'a' && posx>=25) {
-    posx-=2;
+  if (keyIsPressed && keyCode == LEFT_ARROW && posx >= 25) {
+    posx -= 2;
   }
-  if (keyIsPressed && key == 'w' && posy>=25) {
-    posy-=2;
+  if (keyIsPressed && keyCode == UP_ARROW && posy >= 25) {
+    posy -= 2;
   }
-  if (keyIsPressed && key == 's' && posy<=375) {
-    posy+=2;
+  if (keyIsPressed && keyCode == DOWN_ARROW && posy <= height-25) {
+    posy += 2;
   }
 }
