72bf5b1408dd2d4ba86f9fb5c39595b14720e550
ErikOnsager <60490219+ErikOnsager@users.noreply.github.com>
2020-03-16 11:22:08 -0700

    Update sketch.js

--- GrowingEllipse/sketch.js
+++ GrowingEllipse/sketch.js
@@ -1,19 +1,17 @@
-//Some of your code copied due to an infinite for-loop crash in my code, but mine was virtually the same, just with a few tweaks/rewords. 
-
-var x = 250; 
-var y = 250; 
 var size = 30;            //Creating variables for position & size
 var z = 0
 
 let button;
 
 function setup() {       //Creating background
+  createCanvas(600, 600);
   button = createButton('reset'); //creating reset button
-  button.position(455, 478);
+  button.position(width-45, height-22);
   button.mousePressed(reset);
-  createCanvas(500, 500);
   background(255);
   noStroke();
+
+  drawfunc = draw();
 }
 
 function reset(){ //having reset button completely reset the function & wipe the background
@@ -25,18 +23,20 @@ background(255,255,255);
 }
 
 function draw() {
+  x=width/2
+  y=height/2
   background(255,255,255);
   fill((size/2),0,0);       //setting color of ball proportional to size
   ellipse(x, y, size, size);     //creating the ellipse with variables
   distance = dist(x, y, mouseX, mouseY); //distance between ball and mouse
-  if(size<=500&&size>1){         //
+  if(size<=width&&size>1){         //
     if (distance < size / 2) { //if the mouse is on the ball
       size += 3;              //grow the ball to be larger
     } else {
       size = 30;              //if mouse leaves the ball, set back to original size
     }
   }
-  if (size>=500){             //if the ball reaches the edge of the screen
+  if (size>=width){             //if the ball reaches the edge of the screen
     size = 0;                 //make the size of the ball 0
     z = 1;                 //set another variable for a new loop
   }
