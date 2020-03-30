709dffb6059cbbc0808c8215eadf2e831e1a518e
ErikOnsager <60490219+ErikOnsager@users.noreply.github.com>
2020-03-30 10:39:47 -0700

    Update sketch.js

--- RobotProj2/sketch.js
+++ RobotProj2/sketch.js
@@ -7,55 +7,55 @@ function setup() {
   fill(255,0,0);
 }
 
-function drawrobot(var1,var2, arraypos){
-  var XDistance = (mouseX+var1) - PosX[arraypos];
+function drawrobot(var1,var2, a){    //a is just a variable for tracking the position in the array.
+  var XDistance = (mouseX+var1) - PosX[a];
   var XSpeed = (XDistance/10);
-  var YDistance = (mouseY+var2) - PosY[arraypos];
+  var YDistance = (mouseY+var2) - PosY[a];
   var YSpeed = (YDistance/10);
   
   if (abs(XSpeed) > abs(YSpeed) && XSpeed>0){
-    PosY[arraypos]+=random(-1,1)
-    ellipse(PosX[arraypos]+30,PosY[arraypos],60*sin(PosX[arraypos]/30),60);
-    rect(PosX[arraypos],PosY[arraypos],50,100);
-    ellipse(PosX[arraypos]+50,PosY[arraypos]-12,10,10);
+    PosY[a]+=random(-1,1)
+    ellipse(PosX[a]+30,PosY[a],60*sin(PosX[a]/30),60);
+    rect(PosX[a],PosY[a],50,100);
+    ellipse(PosX[a]+50,PosY[a]-12,10,10);
     
-    rect(PosX[arraypos]+20,PosY[arraypos]+95+random(-3,3),20,20);
-    rect(PosX[arraypos]+30,PosY[arraypos]+95+random(-3,3),20,20);
-    line(PosX[arraypos]+45,PosY[arraypos]+60,PosX[arraypos]+45,PosY[arraypos]+10);
+    rect(PosX[a]+20,PosY[a]+95+random(-3,3),20,20);
+    rect(PosX[a]+30,PosY[a]+95+random(-3,3),20,20);
+    line(PosX[a]+45,PosY[a]+60,PosX[a]+45,PosY[a]+10);
   }
   else if (abs(XSpeed) > abs(YSpeed) && XSpeed<0){
-    PosY[arraypos]+=random(-1,1)
-    ellipse(PosX[arraypos]+30,PosY[arraypos],60*sin(PosX[arraypos]/30),60);
-    rect(PosX[arraypos]+10,PosY[arraypos],50,100);
-    ellipse(PosX[arraypos]+10,PosY[arraypos]-12,10,10);
-    rect(PosX[arraypos]+20,PosY[arraypos]+95+random(-3,3),20,20);
-    rect(PosX[arraypos]+10,PosY[arraypos]+95+random(-3,3),20,20);
-    line(PosX[arraypos]+20,PosY[arraypos]+60,PosX[arraypos]+20,PosY[arraypos]+10);
+    PosY[a]+=random(-1,1)
+    ellipse(PosX[a]+30,PosY[a],60*sin(PosX[a]/30),60);
+    rect(PosX[a]+10,PosY[a],50,100);
+    ellipse(PosX[a]+10,PosY[a]-12,10,10);
+    rect(PosX[a]+20,PosY[a]+95+random(-3,3),20,20);
+    rect(PosX[a]+10,PosY[a]+95+random(-3,3),20,20);
+    line(PosX[a]+20,PosY[a]+60,PosX[a]+20,PosY[a]+10);
   }
   else if (abs(YSpeed) > abs(XSpeed) && YSpeed>0){
-    PosY[arraypos]+=random(-1,1)
-    ellipse(PosX[arraypos]+30,PosY[arraypos],60,60);
-    rect(PosX[arraypos],PosY[arraypos],60,100);
-    line(PosX[arraypos],PosY[arraypos],PosX[arraypos]+20,PosY[arraypos]-10);
-    line(PosX[arraypos]+60,PosY[arraypos],PosX[arraypos]+45,PosY[arraypos]-10);
-    line(PosX[arraypos]-40,PosY[arraypos]+60,PosX[arraypos],PosY[arraypos]+10);
-    line(PosX[arraypos]+90,PosY[arraypos]+60,PosX[arraypos]+60,PosY[arraypos]+10);
-    ellipse(PosX[arraypos]+20,PosY[arraypos]-12,10,10);
-    ellipse(PosX[arraypos]+40,PosY[arraypos]-12,10,10);
-    rect(PosX[arraypos]+10,PosY[arraypos]+95+random(-3,3),10,20);
-    rect(PosX[arraypos]+40,PosY[arraypos]+95+random(-3,3),10,20);
+    PosY[a]+=random(-1,1)
+    ellipse(PosX[a]+30,PosY[a],60,60);
+    rect(PosX[a],PosY[a],60,100);
+    line(PosX[a],PosY[a],PosX[a]+20,PosY[a]-10);
+    line(PosX[a]+60,PosY[a],PosX[a]+45,PosY[a]-10);
+    line(PosX[a]-40,PosY[a]+60,PosX[a],PosY[a]+10);
+    line(PosX[a]+90,PosY[a]+60,PosX[a]+60,PosY[a]+10);
+    ellipse(PosX[a]+20,PosY[a]-12,10,10);
+    ellipse(PosX[a]+40,PosY[a]-12,10,10);
+    rect(PosX[a]+10,PosY[a]+95+random(-3,3),10,20);
+    rect(PosX[a]+40,PosY[a]+95+random(-3,3),10,20);
     }
   else if (abs(YSpeed) > abs(XSpeed) && YSpeed<0)   {
-    PosY[arraypos]+=random(-1,1)
-    ellipse(PosX[arraypos]+30,PosY[arraypos],60,60);
-    rect(PosX[arraypos],PosY[arraypos],60,100);
-    line(PosX[arraypos]-40,PosY[arraypos]+60,PosX[arraypos],PosY[arraypos]+10);
-    line(PosX[arraypos]+90,PosY[arraypos]+60,PosX[arraypos]+60,PosY[arraypos]+10);
-    rect(PosX[arraypos]+10,PosY[arraypos]+95+random(-3,3),10,20);
-    rect(PosX[arraypos]+40,PosY[arraypos]+95+random(-3,3),10,20);
+    PosY[a]+=random(-1,1)
+    ellipse(PosX[a]+30,PosY[a],60,60);
+    rect(PosX[a],PosY[a],60,100);
+    line(PosX[a]-40,PosY[a]+60,PosX[a],PosY[a]+10);
+    line(PosX[a]+90,PosY[a]+60,PosX[a]+60,PosY[a]+10);
+    rect(PosX[a]+10,PosY[a]+95+random(-3,3),10,20);
+    rect(PosX[a]+40,PosY[a]+95+random(-3,3),10,20);
   }
-    PosX[arraypos]+=XSpeed;
-  PosY[arraypos]+=YSpeed;
+    PosX[a]+=XSpeed;
+  PosY[a]+=YSpeed;
 }
 
 function draw() {
